1dc0224e2b82e1e06ed13a09c3f5ba8ca3a8f0b1
finsun2020 <57365508+finsun2020@users.noreply.github.com>
2019-11-05 13:47:12 +0100

    Add files via upload

---
+++ Capstone_project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project Notebook
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
